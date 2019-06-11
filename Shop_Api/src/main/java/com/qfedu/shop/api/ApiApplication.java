28daebbe5943bd922b688544a9b5a7693e8849b5
zuzu <zuzu_1991@978049543.com>
2019-06-11 19:35:28 +0800

    一款电子商务平台项目

---
+++ Shop_Api/src/main/java/com/qfedu/shop/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package com.qfedu.shop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ApiApplication  extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApiApplication.class);
+    }
+}
---
+++ Shop_Common/src/main/java/com/qfedu/common/T.java
@@ -0,0 +1,4 @@
+package com.qfedu.common;
+
+public class T {
+}
